f0a67457a28d9e9ab455cce150550deb1387bd3b
Aymen Smati <ayman@Smatis-MacBook-Pro.local>
2021-08-20 15:31:14 +0100

    mockup iphone ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState , Component} from 'react'
+import { render } from "react-dom";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import {
+  DraftailEditor,
+  createEditorStateFromRaw,
+  serialiseEditorStateToRaw
+} from "draftail";
+class EditorContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+  }
+
+  onEditorStateChange = (editorState) => {
+    // console.log(editorState)
+    this.setState({
+      editorState
+    });
+  };
+
+  render() {
+    const { editorState } = this.state;
+    console.log(editorState.getBlockTree());
+    return (
+      <div className="editor">
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={this.onEditorStateChange}
+          
+        />
+         {JSON.stringify(serialiseEditorStateToRaw(editorState))}
+
+      </div>
+    );
+  }
+}
+
 
 function App() {
+
+  const [text,setText]=useState("");
+  console.log(text)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div >
+<EditorContainer/>
+
+<div className='input'>
+<input onChange={(event)=>setText(event.target.value)} value={text} type="text"></input>
+</div>
+<div className='right'>
+
+<div class="iphone">
+<div className="i">
+</div>
+<div className="b">
+</div>
+<div className='text'>
+{text}
+</div>
+</div>
+</div>
+</div>
   );
 }
 
